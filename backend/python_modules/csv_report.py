b654ba2df2ab1c0940cc181d795f76b10a8b6d43
p2pahuja <p2pahuja@uwaterloo.ca>
2025-05-14 14:04:13 -0400

    add csv report

--- backend/python_modules/csv_report.py
+++ backend/python_modules/csv_report.py
@@ -1,3 +1,4 @@
+from progress.bar import Bar as Bar
 import csv
 import python_modules.time_helper as time_helper
 import python_modules.logfo as logfo
@@ -11,13 +12,18 @@ class CsvReport():
         if len(data['parameters']) == 0:
             data['parameters'] = self.instance.get_device(data['id'])[
                 'parameters']
-        self.add_data(data['id'], data['day_start'], data['day_end'],
+            
+        return self.add_data(data['id'], data['day_start'], data['day_end'],
                       data['interval'], data['func'], data['parameters'])
 
 
     def add_data(self, device_id, day_start, day_end, interval, func, parameters, legend='', xlabel_rotation=90, x_label='Time'):
+        time_only = False
+        if day_start == day_end:
+            time_only = True
+            
         data = self.instance.get_report_mix(
-            device_id, day_start, day_end, interval, func, parameters)
+            device_id, day_start, day_end, interval, func, parameters,unix_time=time_only)
         logfo.logi("Report CSV","Generating")
         # self.bar = Bar('Generating CSV Report')
         # self.bar.max = len(data) * (len(parameters) + 1) + 3
@@ -30,15 +36,12 @@ class CsvReport():
         # Table headers
         self.headers = []
         self.rows = []
-        parameters = []
-        for p in params:
-            parameters += p
-        for parameter in parameters:    
+        for parameter in params:    
             self.headers.append(parameter)
         self.headers.append('Timestamp')
         for row in data:
             r = []
-            for parameter in parameters:
+            for parameter in params:
                 r.append(row[parameter])
                 # self.bar.next()
             self.rows.append(r+[row['timestamp']])
@@ -52,7 +55,7 @@ class CsvReport():
         
 
     def save(self, filename, q=csv.QUOTE_NONE):
-        with open("./Reports/" + filename, 'w', newline="") as file:
+        with open("Reports " + filename, 'w', newline="") as file:
             csvwriter = csv.writer(file, quoting=q) # 2. create a csvwriter object
             csvwriter.writerow(self.headers) # 4. write the header
             csvwriter.writerows(self.rows) # 5. write the rest of the data
